43a7be7a7023967846a3563dbd0b06a54c699f9c
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-02-10 10:57:31 -0800

    add model for product

---
+++ src/gummi-bear-kingdom/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GummiBearKingdom.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string CountryOfOrigin { get; set; }
+    }
+}
