c76c53a35fc070421a34f6e0915f990be285901b
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-02-10 13:58:34 -0800

    Implement update

--- src/gummi-bear-kingdom/Controllers/ProductsController.cs
+++ src/gummi-bear-kingdom/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GummiBearKingdom.Models;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace GummiBearKingdom.Controllers
@@ -31,6 +32,20 @@ namespace GummiBearKingdom.Controllers
         public IActionResult Create(Product product)
         {
             db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+            return View(thisProduct);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            db.Entry(product).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
