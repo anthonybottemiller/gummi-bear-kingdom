4fc610d773d62cb045c3bedebad1233c03a74f4a
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-02-10 11:32:50 -0800

    add basic controller for products

---
+++ src/gummi-bear-kingdom/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GummiBearKingdom.Models;
+
+
+namespace GummiBearKingdom.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private GummiBearKingdomContext db = new GummiBearKingdomContext();
+        public IActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+        public IActionResult Create(Product product)
+        {
+            db.Products.Add(product);
+            
+            return View();
+        }
+    }
+}
