0e9337113547ea2220b7789a223c8503b760ccbc
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-02-10 13:26:33 -0800

    Implement Delete for products

--- src/gummi-bear-kingdom/Controllers/ProductsController.cs
+++ src/gummi-bear-kingdom/Controllers/ProductsController.cs
@@ -34,5 +34,20 @@ namespace GummiBearKingdom.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public IActionResult Delete(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+            return View(thisProduct);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+            db.Products.Remove(thisProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
